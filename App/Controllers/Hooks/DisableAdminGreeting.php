0edea1a1855e3f84a244f670288cd1f63760083e
ve3 <kawewong@gmail.com>
2026-07-20 22:59:18 +0700

    Update disable admin greeting to support many languages.

--- App/Controllers/Hooks/DisableAdminGreeting.php
+++ App/Controllers/Hooks/DisableAdminGreeting.php
@@ -16,32 +16,22 @@ if (!class_exists('\\RundizableWpFeatures\\App\\Controllers\\Hooks\\DisableAdmin
      * 
      * @since 0.2.7
      */
-    class DisableAdminGreeting implements \RundizableWpFeatures\App\Controllers\ControllerInterface
+    class DisableAdminGreeting extends \RundizableWpFeatures\App\Controllers\Hooks\BasedHooks
     {
 
 
-        use \RundizableWpFeatures\App\AppTrait;
-
-
         /**
-         * Class constructor.
+         * Register hooks per this class only.
+         * 
+         * @since 1.0.7 Renamed from `registerHooks()`.
          */
-        public function __construct()
-        {
-            $this->getOptions();
-        }// __construct
-
-
-        /**
-         * {@inheritDoc}
-         */
-        public function registerHooks()
+        public function perClassRegisterHooks()
         {
             global $rundizable_wp_features_optname;
             if (isset($rundizable_wp_features_optname['disable_admin_greeting']) && strval($rundizable_wp_features_optname['disable_admin_greeting']) === '1') {
                 add_action('wp_before_admin_bar_render', [$this, 'removeHowdy']);
             }
-        }// registerHooks
+        }// perClassRegisterHooks
 
 
         /**
@@ -55,7 +45,7 @@ if (!class_exists('\\RundizableWpFeatures\\App\\Controllers\\Hooks\\DisableAdmin
             global $wp_admin_bar;
 
             $myAccountNode = $wp_admin_bar->get_node('my-account');
-            $myAccountNode->title = preg_replace('/(\w+),(\s+)/u', '', $myAccountNode->title);
+            $myAccountNode->title = preg_replace('/^.*?<span class="display-name"/s', '<span class="display-name"', $myAccountNode->title);
             $wp_admin_bar->remove_node('my-account');
             $wp_admin_bar->add_node($myAccountNode);
             unset($myAccountNode);
