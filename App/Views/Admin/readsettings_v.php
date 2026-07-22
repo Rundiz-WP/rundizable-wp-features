e8e5b23c3517cdfbb55853427bc3ded308c3c3cd
ve3 <kawewong@gmail.com>
2026-07-23 06:22:21 +0700

    Comment out unused readsettings_v.php file. This views is for debugging only.

--- App/Views/Admin/readsettings_v.php
+++ App/Views/Admin/readsettings_v.php
@@ -1,17 +1,39 @@
 <?php // phpcs:disable
+
+/*
+if (!defined('ABSPATH')) {
+    exit();
+}
+
+
 ?>
 <div class="wrap">
     <h1><?php _e('Example of how to access settings values in option db.', 'rundizable-wp-features'); ?></h1>
 
     <ol>
-        <li><?php printf(__('Call to <code>%s</code>', 'rundizable-wp-features'), '$this->getOptions();'); ?></li>
-        <li><?php printf(__('Access this variable as global <code>%s</code>. This variable will be change, up to config in AppTrait.', 'rundizable-wp-features'), 'global $rundizable_wp_features_optname;'); ?></li>
-        <li><?php _e('Now, you can use this variable to access its array key anywhere.', 'rundizable-wp-features'); ?></li>
+        <li><?php printf(
+            // translators: %s PHP code.
+            esc_html__('Call to %s', 'rundizable-wp-features'),
+            '<code>$this->getOptions();</code>'
+        ); ?></li>
+        <li><?php printf(
+            // translators: %s PHP code.
+            esc_html__('Access this variable as global %s. This variable will be change, up to config in AppTrait.', 'rundizable-wp-features'),
+            '<code>global $plugin_template_optname;</code>'
+        ); ?></li>
+        <li><?php esc_html_e('Now, you can use this variable to access its array key anywhere.', 'rundizable-wp-features'); ?></li>
     </ol>
     <h3>Example: <code>print_r($rundizable_wp_features_optname);</code></h3>
     <pre style="background-color: #333; border: 1px solid #ccc; color: #ddd; height: 500px; overflow: auto; padding: 10px;"><?php 
-        if (isset($rundizable_wp_features_optname)) {
-            echo htmlspecialchars(print_r($rundizable_wp_features_optname, true), ENT_QUOTES, get_option('blog_charset')); 
-        }
+    if (isset($rundizable_wp_features_optname)) {
+        echo esc_html(print_r($rundizable_wp_features_optname, true)); // phpcs:ignore WordPress.PHP.DevelopmentFunctions
+    }
     ?></pre>
+    <p><?php esc_html_e('To see real data before escape, please use view source to see them below this line.', 'rundizable-wp-features'); ?></p>
+    <!--
+    raw data from DB (below this line):
+    <?php print_r($plugin_template_optname); // phpcs:ignore WordPress.PHP.DevelopmentFunctions ?> 
+    -->
 </div>
+<?php
+/**/
